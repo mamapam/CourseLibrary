0d7b040785d373c02e3f59ce587cb52b126197d5
mamapam <mamapam.dev@gmail.com>
2020-11-05 22:43:33 -0500

    Created controller for authors

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+	[ApiController]
+	public class AuthorsController : ControllerBase
+	{
+		private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+		public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+		{
+			_courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+		}
+
+		public IActionResult GetAuthors()
+		{
+			var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+			return new JsonResult(authorsFromRepo);
+		}
+	}
+}
