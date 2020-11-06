eaea6a1226b6ddf5464dc3115d9676bb10b54f79
mamapam <mamapam.dev@gmail.com>
2020-11-06 08:49:58 -0500

    Added automapper, created profile to use for it

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
@@ -35,6 +36,8 @@ namespace CourseLibrary.API
 				setupAction.ReturnHttpNotAcceptable = true;
 
 			}).AddXmlDataContractSerializerFormatters();
+
+			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 			services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
