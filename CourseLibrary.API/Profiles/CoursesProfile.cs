5bacf75aa1c33ed002ed78cfd4f08670bdaa67d6
mamapam <mamapam.dev@gmail.com>
2020-12-29 17:35:01 -0500

    Created DTO and mapping profile

---
+++ CourseLibrary.API/Profiles/CoursesProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Profiles
+{
+	public class CoursesProfile : Profile
+	{
+		CreateMap<Entities.Course, Models.CourseDto>();
+	}
+}
